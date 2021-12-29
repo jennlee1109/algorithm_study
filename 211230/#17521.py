84fb4cf260e7169eddb7d0d43019e6e1dbd08d55
jennlee1109 <ljenn0911@gmail.com>
2021-12-30 01:46:57 +0900

    하루 전보다 크면 매도, 하루 뒤보다 작으면 매입

--- 211230/#17521.py
+++ 211230/#17521.py
@@ -10,32 +10,17 @@ cash = w
 for _ in range(n):
     price = int(input())
     coin_prices.append(price)
-    if max_coin < price:
-        max_coin = price
-    if min_coin > price:
-        min_coin = price
 
-# print(coin_prices, max_coin, min_coin)
+for day in range(n-1):
+    if coin_prices[day] < coin_prices[day+1]:
+        coin += cash // coin_prices[day]
+        cash = cash % coin_prices[day]
 
-for day in range(n):
-    if day == 0:
-        if coin_prices[day] <= coin_prices[day+1] and coin_prices[day] < max_coin:
-            coin += cash // coin_prices[day]
-            cash -= coin * coin_prices[day]
+    elif coin_prices[day] > coin_prices[day-1]:
+        cash += coin * coin_prices[day]
+        coin = 0
 
-    elif day == n-1:
-        if coin_prices[day] >= coin_prices[day-1] and coin_prices[day] > min_coin:
-            cash += coin_prices[day] * coin
-            coin = 0
-
-    else:
-        if coin_prices[day-1] < coin_prices[day] and coin_prices[day] > coin_prices[day+1]:
-            cash += coin_prices[day] * coin
-            coin = 0
-        elif coin_prices[day-1] > coin_prices[day] and coin_prices[day] < coin_prices[day+1]:
-            coin += cash // coin_prices[day]
-            cash -= coin * coin_prices[day]
-
-    # print(day+1, cash, coin)
+if coin > 0:
+    cash += coin * coin_prices[-1]
 
 print(cash)
