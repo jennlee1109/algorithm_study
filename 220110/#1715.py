7507a15ef19a2e23b92d63b217b5f23164569a3e
jennlee1109 <ljenn0911@gmail.com>
2022-01-11 01:21:52 +0900

    중간 묶음 합친  결과를 최소힙으로 정렬 후 재묶음

--- 220110/#1715.py
+++ 220110/#1715.py
@@ -1,33 +1,17 @@
+import heapq
+
 n = int(input())
 
-cards = [int(input()) for _ in range(n)]
+heap = []
+for _ in range(n):
+    heapq.heappush(heap, int(input()))
+result = 0
 
-cards.sort()
-result = []
-total = 0
-tmp = cards[0]
+while len(heap) > 1:
+    total = heapq.heappop(heap) + heapq.heappop(heap)
+    # print(total)
+    result += total
+    heapq.heappush(heap, total)
+    # print(heap)
 
-for i in range(1, n):
-    tmp += cards[i]
-    total += tmp
-
-result.append(total)
-
-idx = 0
-tmp = 0
-total = 0
-
-for i in range(n):
-    if i % 2 == 0 or i == n-1:
-        if i == n-1:
-            tmp += cards[i]
-        total += tmp * (n // 2)
-        tmp = cards[i]
-    else:
-        tmp += cards[i]
-
-
-result.append(total)
 print(result)
-
-print(min(result))
