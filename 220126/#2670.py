7db8158ec8d3cb0e00ec25f608362fbfdc9c41b7
jennlee1109 <ljenn0911@gmail.com>
2022-01-26 11:17:39 +0900

    dp 이용해 이전값과의 곱한 결과와 현재값 비교

--- 220126/#2670.py
+++ 220126/#2670.py
@@ -1,15 +1,9 @@
 n = int(input())
 num_list = [float(input()) for _ in range(n)]
 
-result = num_list[0]
-for i in range(n):
-    for j in range(i, n):
-        if num_list[i] <= num_list[j]:
-            total = num_list[i]
-            for k in range(i+1, j+1):
-                total *= num_list[k]
-            # print(num_list[i:j+1], result, total)
-            if result <= total:
-                result = total
+dp = [num_list[0]]
 
-print(round(result, 4))
+for i in range(1, n):
+    dp.append(max(num_list[i], dp[i-1] * num_list[i]))
+
+print("{0:.3f}".format(max(dp)))
