4ec1701d8c2f5ae32b711e220ae1e8136aa4c5fa
jennlee1109 <ljenn0911@gmail.com>
2022-02-21 15:15:43 +0900

    일일이 계산X. 방향 같을때 경우의 수 계산

--- 220221/#5874.py
+++ 220221/#5874.py
@@ -2,20 +2,14 @@ import sys
 
 path = sys.stdin.readline()
 
-N = len(path)
-x = []
-y = []
-
-for i in range(N - 1):
-    if path[i] == '(' and path[i + 1] == '(':
-        x.append((i, i + 1))
-    elif path[i] == ')' and path[i + 1] == ')':
-        y.append((i, i + 1))
-
 result = 0
-for i in range(len(x)):
-    for j in range(len(y)):
-        if x[i][0] < y[j][0] and x[i][1] < y[j][1]:
-            result += 1
+cnt = 0
+
+for i in range(len(path)-1):
+    if path[i] == path[i+1]:
+        if path[i] == '(':
+            cnt += 1
+        else:
+            result += cnt
 
 print(result)
